ccba7daf08ae857775b51676935a7fdef03fcb8a
Feodor Holkin <122164805+FeodorH@users.noreply.github.com>
2026-07-21 09:10:37 +0300

    Update: linked-list-cycle-detection - submission-2

--- Data Structures & Algorithms/linked-list-cycle-detection/submission-2.kt
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.kt
@@ -4,24 +4,18 @@
  *     var next: ListNode? = null
  * }
  */
-
-class Solution {
+class Solution{
     fun hasCycle(head: ListNode?): Boolean {
-        if(head == null) return false
-        if(head?.next === head) return true
-        var el1 : ListNode? = head
-        var el2 : ListNode? = head?.next
-        while(el2 != null && el2.next != null && el1!=null){
-            if(el1 === el2){
-                return true
+        var slow = head
+        var fast = head
+        do {
+            if(slow?.next != null && fast?.next != null && fast?.next?.next != null){
+                slow = slow?.next
+                fast = fast?.next?.next
+            }else{
+                return false
             }
-            el1 = el1?.next
-            el2 = el2?.next?.next
-        }
-        return false
+        } while(slow != fast)
+        return true
     }
-
-//    class ListNode(var `val`: Int) {
-//        var next: ListNode? = null
-//    }
 }
